713e8a09f7bb5e14ae2a62f06437389fb87f68ef
Yves Baumes <ybaumes@gmail.com>
2015-01-02 17:33:28 +0100

    Refactoring.

--- test.cpp
+++ test.cpp
@@ -4,6 +4,7 @@
 #include <Fl/Fl_Box.H>
 #include <Fl/Fl_Input.H>
 #include <iostream>
+#include <Fl/Fl_Text_Editor.H>
 
 using namespace std;
 
@@ -12,12 +13,16 @@ using namespace std;
 int main(int argc, char *argv[])
 {
    Fl_Window w (200, 200);
+
    // new Fl_Button(100, 100, 30, 30, "Okay");
+
    Fl_Box *box = new Fl_Box(10,10, 60,40, "Hello");
    box->box(FL_UP_BOX);
    box->labelfont(FL_BOLD);
+
    Fl_Input *input = new Fl_Input(40, 50, 60, 40, "label");
    input->value("Now...");
+
    w.end();
    w.show(argc, argv);
    Fl::run();
