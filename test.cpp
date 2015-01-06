65375a0daaf65490595c2db09faefa946a7d13cf
Yves Baumes <ybaumes@gmail.com>
2015-01-06 11:49:27 +0100

    Premise for a text editor.

--- test.cpp
+++ test.cpp
@@ -5,29 +5,33 @@
 #include <Fl/Fl_Input.H>
 #include <iostream>
 #include <Fl/Fl_Text_Editor.H>
+#include <Fl/Fl_Text_Buffer.H>
 
 using namespace std;
 
 // eval $(fltk-config --compile test.cpp)
 
+void changed_cb(int pos, int nInserted, int nDeleted,
+                int nRestyled, const char* deletedText,
+                void* cbArg)
+{
+    Fl_Text_Buffer *buf = (Fl_Text_Buffer*)cbArg;
+    buf->savefile("/tmp/test.txt");
+}
+
 int main(int argc, char *argv[])
 {
-   Fl_Window w (200, 200);
+   Fl_Window w (600, 400);
 
-   // new Fl_Button(100, 100, 30, 30, "Okay");
-
-   Fl_Box *box = new Fl_Box(10,10, 60,40, "Hello");
-   box->box(FL_UP_BOX);
-   box->labelfont(FL_BOLD);
-
-   Fl_Input *input = new Fl_Input(40, 50, 60, 40, "label");
-   input->value("Now...");
-
-   Fl_Text_Editor *ed = new Fl_Text_Editor(120, 50, 60, 70, "editor");
+   Fl_Text_Editor *ed = new Fl_Text_Editor(5, 5, 590, 390, "editor");
+   Fl_Text_Buffer *textBuf = new Fl_Text_Buffer();
+   textBuf->add_modify_callback(changed_cb, textBuf/*cbArg*/);
+   ed->buffer(textBuf);
+   ed->textfont(FL_COURIER);
 
    w.end();
    w.show(argc, argv);
    Fl::run();
-   cout << input->value() << endl;
+   cout << textBuf->text() << endl;
    return 0;
 }
