3e7582e012f851711c65e1f0605baa720af3a44a
Yves Baumes <ybaumes@gmail.com>
2015-01-06 17:26:53 +0100

    Implement file switching.

--- test.cpp
+++ test.cpp
@@ -1,30 +1,37 @@
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
-// #include <FL/Fl_Button.H>
 #include <Fl/Fl_Box.H>
 #include <Fl/Fl_Input.H>
 #include <iostream>
 #include <Fl/Fl_Text_Editor.H>
 #include <Fl/Fl_Text_Buffer.H>
+#include <Fl/Fl_File_Chooser.H>
 
 using namespace std;
 
 // eval $(fltk-config --compile test.cpp)
+
+string filename;
 
 void changed_cb(int pos, int nInserted, int nDeleted,
                 int nRestyled, const char* deletedText,
                 void* cbArg)
 {
     Fl_Text_Buffer *buf = (Fl_Text_Buffer*)cbArg;
-    buf->savefile("/tmp/test.txt");
+    buf->savefile(filename.c_str());
 }
 
 int main(int argc, char *argv[])
 {
-    Fl_Window w (600, 400);
+    filename = "/tmp/test.txt";
+    // Fl_File_Chooser chooser ("/tmp/test.txt", "*", 0, "Open File");
+//    chooser.end();
+    // chooser.show();
 
+    Fl_Window w (600, 400);
     Fl_Text_Editor *ed = new Fl_Text_Editor(5, 5, 590, 390, "editor");
     Fl_Text_Buffer *textBuf = new Fl_Text_Buffer();
+    textBuf->loadfile(filename.c_str());
     textBuf->add_modify_callback(changed_cb, textBuf/*cbArg*/);
     ed->buffer(textBuf);
     ed->textfont(FL_COURIER);
